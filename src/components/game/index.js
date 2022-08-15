57075ce3a88c718a3aed27050d16de21fb9ed1f5
Erdem Ceylan <erdemxceylan@gmail.com>
2022-08-15 04:34:52 +0300

    fonts are implemented to the game page

--- src/components/game/index.js
+++ src/components/game/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import './styles.scss';
 import * as svgs from '../../assets/svgs';
 
+const choices = [49, 64, 56];
+const legend = [{ title: 'Puan', value: 120 }, { title: 'Tur', value: 2 }, { title: 'Soru', value: 7 },];
+const question = { first: 7, operation: 'x', second: 8 };
+
 export default function Game() {
+
    return (
-      <div>
-         {svgs.schema}
-         {svgs.face.thinking}
-         {/* {svgs.face.sad} */}
-         {/* {svgs.face.happy} */}
-      </div>
+      <>
+         <div>
+            {svgs.schema}
+            {svgs.face.thinking}
+            {/* {svgs.face.sad} */}
+            {/* {svgs.face.happy} */}
+            {legend.map((item, index) => <p className='legend' key={`legend-${index}`}>{item.title}: {item.value}</p>)}
+            {choices.map((choice, index) => <p className='choice' key={`choice-${index}`}>{choice}</p>)}
+            <p className='question'>{`${question.first} ${question.operation} ${question.second}`}</p>
+         </div>
+         {svgs.check}
+         <div style={{ height: '20rem' }}></div>
+      </>
    );
 }
