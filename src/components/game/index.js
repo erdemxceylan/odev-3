764a63b68db936f3b69240855b5ccd6a08a42628
Erdem Ceylan <erdemxceylan@gmail.com>
2022-08-15 04:44:48 +0300

    answer state is defined

--- src/components/game/index.js
+++ src/components/game/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
 import * as svgs from '../../assets/svgs';
 
 const choices = [49, 64, 56];
 const legend = [{ title: 'Puan', value: 120 }, { title: 'Tur', value: 2 }, { title: 'Soru', value: 7 },];
 const question = { first: 7, operation: 'x', second: 8 };
+const CORRECT = 'Correct';
+const INCORRECT = 'Incorrect';
 
 export default function Game() {
+   const [answer, setAnswer] = useState(INCORRECT);
 
    return (
       <>
          <div>
             {svgs.schema}
-            {svgs.face.thinking}
-            {/* {svgs.face.sad} */}
-            {/* {svgs.face.happy} */}
+            {answer === CORRECT ? svgs.face.happy : answer === INCORRECT ? svgs.face.sad : svgs.face.thinking}
             {legend.map((item, index) => <p className='legend' key={`legend-${index}`}>{item.title}: {item.value}</p>)}
             {choices.map((choice, index) => <p className='choice' key={`choice-${index}`}>{choice}</p>)}
             <p className='question'>{`${question.first} ${question.operation} ${question.second}`}</p>
